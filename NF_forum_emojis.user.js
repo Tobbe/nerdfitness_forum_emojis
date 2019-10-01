9515c675c1d3340a311a96377917f3d36ed3c6ca
Tobbe Lundberg <tobbe@tlundberg.com>
2019-10-01 21:26:21 +0100

    initial commit

---
+++ NF_forum_emojis.user.js
@@ -0,0 +1,142 @@
+// ==UserScript==
+// @name         NerdFitness forum emojis
+// @namespace    https://github.com/tobbe
+// @version      0.1
+// @description  Emojis for use on the NF forums
+// @author       Tobbe Lundberg
+// @match        https://rebellion.nerdfitness.com/index.php?/topic/*
+// @match        https://rebellion.nerdfitness.com/index.php?/forum/*/&do=add
+// @exclude      https://rebellion.nerdfitness.com/index.php?/topic/*&do=embed
+// @exclude      https://rebellion.nerdfitness.com/index.php?/topic/*&do=embed&*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function appendImg(element, name, src) {
+        const id = '_' + Math.random().toString(36).substr(2, 9) + '_';
+        const containerDiv = document.createElement('span');
+        containerDiv.className = 'emoji-container';
+        const img = document.createElement('img');
+        img.setAttribute('src', src);
+        img.setAttribute('alt', name);
+        img.id = id
+        containerDiv.appendChild(img);
+
+        const clonedImg = img.cloneNode();
+        clonedImg.id = id + 'clone';
+
+        element.appendChild(containerDiv);
+
+        const cloneContainerSpan = document.createElement('span');
+        cloneContainerSpan.appendChild(clonedImg);
+
+        document.getElementById('emoji-bar-hidden').appendChild(cloneContainerSpan);
+    }
+
+    function SelectText(element) {
+        if (document.body.createTextRange) {
+            const range = document.body.createTextRange();
+            range.moveToElementText(element);
+            range.select();
+        } else if (window.getSelection) {
+            const selection = window.getSelection();
+            const range = document.createRange();
+            range.selectNodeContents(element);
+            selection.removeAllRanges();
+            selection.addRange(range);
+        }
+    }
+
+    const div = document.createElement('div');
+    div.style.cssText = `
+        position: fixed;
+        width: 100px;
+        height: 200px;
+        top: 100px;
+        right: 0;
+        border: 1px solid black;
+        background-color: rgba(135, 206, 250, 0.3);
+        z-index: 30000;
+    `;
+    div.id = 'emoji-bar';
+    document.body.appendChild(div);
+
+    const hiddenDivContainer = document.createElement('div');
+    hiddenDivContainer.style.cssText = `
+        overflow: hidden;
+        background-color: transparent;
+        position: absolute;
+        top: 0;
+        left: 0;
+        border: 10px solid blue;
+    `;
+    document.body.appendChild(hiddenDivContainer);
+
+    const divHidden = document.createElement('div');
+    divHidden.style.cssText = `
+        position: absolute;
+        width: 100px;
+        height: 200px;
+        top: 0;
+        right: -200px;
+        border: 3px solid black;
+        background-color: transparent;
+    `;
+    divHidden.id = 'emoji-bar-hidden';
+    hiddenDivContainer.appendChild(divHidden);
+
+    appendImg(div, 'Thumbs Up', 'https://i.imgur.com/SyBNiWx.png');
+    appendImg(div, 'Vomit', 'https://i.imgur.com/0zZO3Vz.png');
+    appendImg(div, 'Shrug', 'https://i.imgur.com/coaLGWL.png');
+    appendImg(div, 'Hug', 'https://i.imgur.com/tbRniFi.png');
+    appendImg(div, 'Strong', 'https://i.imgur.com/X6ZKbOO.png');
+    appendImg(div, 'Weight Lifting', 'https://i.imgur.com/4OHBWJ9.png');
+    appendImg(div, 'Thinking', 'https://i.imgur.com/tjDxdYl.png');
+    appendImg(div, 'Plate', 'https://i.imgur.com/qwCJ0K4.png');
+    appendImg(div, 'Fire', 'https://i.imgur.com/QZH0gwD.png');
+    appendImg(div, 'Rolling on the Floor Laughing', 'https://i.imgur.com/DGB6wKK.png');
+    appendImg(div, 'Yummy', 'https://i.imgur.com/sDOcBS5.png');
+    appendImg(div, 'Note', 'https://i.imgur.com/vJrNCMo.png');
+    appendImg(div, 'Running', 'https://i.imgur.com/mMsKd8u.png');
+    appendImg(div, 'Book', 'https://i.imgur.com/JhvxQlU.png');
+    appendImg(div, 'Blush', 'https://i.imgur.com/bIksj8K.png');
+    appendImg(div, 'Love Eyes', 'https://i.imgur.com/z9Ejuii.png');
+    appendImg(div, 'Cry', 'https://i.imgur.com/NuOz8aE.png');
+    appendImg(div, 'Bomb', 'https://i.imgur.com/25tmYNs.png');
+    appendImg(div, 'Lab tube', 'https://i.imgur.com/9hgp1JX.png');
+    appendImg(div, 'Sweden', 'https://i.imgur.com/pNSnuMc.png');
+    appendImg(div, 'Star', 'https://i.imgur.com/G4X1TOL.png');
+    appendImg(div, 'Medal', 'https://i.imgur.com/mVwiKqY.png');
+    appendImg(div, 'Poo', 'https://i.imgur.com/6QUVSkH.png');
+    appendImg(div, 'Check', 'https://i.imgur.com/n2GokI9.png');
+
+    document.querySelectorAll('#emoji-bar .emoji-container').forEach(element => {
+        element.onclick = e => {
+            let container = e.target;
+            let id = '';
+            if (container.tagName.toLowerCase() === 'img') {
+                console.log('clicked img', e.target.id);
+                id = e.target.id;
+                container = e.target.parentNode;
+
+                console.log('DEBUG', container.querySelector('img').id);
+            } else {
+                console.log('clicked img parent', e.target.querySelector('img').id);
+                id = e.target.querySelector('img').id
+            }
+
+            const hiddenEmoji = document.getElementById(id + 'clone');
+            console.log('hidden emoji', hiddenEmoji.getAttribute('alt'));
+            const hiddenContainer = hiddenEmoji.parentNode;
+
+            console.log('click', container);
+            hiddenContainer.setAttribute('contenteditable', true);
+            SelectText(hiddenContainer);
+            document.execCommand('copy');
+            window.getSelection().removeAllRanges();
+            hiddenContainer.removeAttribute('contenteditable');
+        };
+    });
+})();
